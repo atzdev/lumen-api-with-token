08dabe7a0af888770bbb5779619e81a868e34555
mookemjung <artzpluz@gmail.com>
2018-04-10 13:45:25 +0700

    lumen api with api_token

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+
+      $rules = [
+          'email' => 'required',
+          'password' => 'required'
+      ];
+
+        $customMessages = [
+           'required' => ':attribute must not empty'
+      ];
+        $this->validate($request, $rules, $customMessages);
+         $email    = $request->input('email');
+        try {
+            $login = User::where('email', $email)->first();
+            //dd($login);
+            if ($login) {
+                
+                    if (Hash::check($request->input('password'), $login->password)) {
+                      //dd('password ok');
+                        try {
+                            $api_token = sha1($login->id.time());
+                              //dd($api_token);
+                              $create_token = User::where('id', $login->id)->update(['api_token' => $api_token]);
+                              
+                              $res['status'] = true;
+                              $res['message'] = 'Success login';
+                              $res['data'] =  $login;
+                              $res['api_token'] =  $api_token;
+
+                              return response($res, 200);
+
+
+                        } catch (\Illuminate\Database\QueryException $ex) {
+                            $res['status'] = false;
+                            $res['message'] = $ex->getMessage();
+                            return response($res, 500);
+                        }
+                    } else {
+                        $res['success'] = false;
+                        $res['message'] = 'Username / email / password not found';
+                        return response($res, 401);
+                    }
+                
+            } else {
+                $res['success'] = false;
+                $res['message'] = 'Username / email / password not found';
+                return response($res, 401);
+            }
+        } catch (\Illuminate\Database\QueryException $ex) {
+            $res['success'] = false;
+            $res['message'] = $ex->getMessage();
+            return response($res, 500);
+        }
+    }
+}
